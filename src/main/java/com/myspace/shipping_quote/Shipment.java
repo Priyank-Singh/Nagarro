2e44b79856a313636810eaaf004117dcfef29713
rhpamAdmin <>
2022-06-09 13:57:31 +0530

    {/src/main/java/com/myspace/shipping_quote/Shipment.java}

--- src/main/java/com/myspace/shipping_quote/Shipment.java
+++ src/main/java/com/myspace/shipping_quote/Shipment.java
@@ -6,12 +6,65 @@ package com.myspace.shipping_quote;
 
 public class Shipment implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Shipment() {
-    }
+	private java.lang.Integer distance;
+	private java.lang.String from;
+	private java.lang.Integer id;
+	private java.lang.String to;
+	private java.lang.Boolean usesCustoms;
 
+	public Shipment() {
+	}
 
+	public java.lang.Integer getDistance() {
+		return this.distance;
+	}
 
+	public void setDistance(java.lang.Integer distance) {
+		this.distance = distance;
+	}
+
+	public java.lang.String getFrom() {
+		return this.from;
+	}
+
+	public void setFrom(java.lang.String from) {
+		this.from = from;
+	}
+
+	public java.lang.Integer getId() {
+		return this.id;
+	}
+
+	public void setId(java.lang.Integer id) {
+		this.id = id;
+	}
+
+	public java.lang.String getTo() {
+		return this.to;
+	}
+
+	public void setTo(java.lang.String to) {
+		this.to = to;
+	}
+
+	public java.lang.Boolean getUsesCustoms() {
+		return this.usesCustoms;
+	}
+
+	public void setUsesCustoms(java.lang.Boolean usesCustoms) {
+		this.usesCustoms = usesCustoms;
+	}
+
+	public Shipment(java.lang.Integer distance, java.lang.String from,
+			java.lang.Integer id, java.lang.String to,
+			java.lang.Boolean usesCustoms) {
+		this.distance = distance;
+		this.from = from;
+		this.id = id;
+		this.to = to;
+		this.usesCustoms = usesCustoms;
+	}
 
 }
